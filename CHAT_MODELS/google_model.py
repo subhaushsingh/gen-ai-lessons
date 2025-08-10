c12555ee7b5814326758ffbcb01dbbe773daa05a
Subh <subhaushsingh@gmail.com>
2025-08-10 22:02:09 +0530

    used pydantic for structured output parser

--- CHAT_MODELS/google_model.py
+++ CHAT_MODELS/google_model.py
@@ -5,10 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-model = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+#model = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
 
-
-result = model.invoke('What is the capital of india?')
+result = llm.invoke('What is the capital of india?')
 
 print(result)
 print('------------------------')
