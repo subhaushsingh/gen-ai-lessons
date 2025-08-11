455cecab7b6b8ab26157c3aa63900260f2ce5055
Subh <subhaushsingh@gmail.com>
2025-08-12 00:05:22 +0530

    used jsonOutputParser with chain

---
+++ OUTPUT_PARSER/jsonoutputparser.py
@@ -0,0 +1,35 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation",
+    
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template="Give me the name, age and city of a fictional person \n {format_instruction}",
+    input_variables=[],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+# prompt = template.format()
+
+# result = model.invoke(prompt)
+
+# final_result = parser.parse(result.content)
+
+chain = template | model | parser
+
+final_result = chain.invoke({})
+
+print(final_result)
