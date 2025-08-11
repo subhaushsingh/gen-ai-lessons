4d133913adb709879e689c57593d6c84b56968b8
Subh <subhaushsingh@gmail.com>
2025-08-11 23:31:25 +0530

    working with strOutputParser using chain

--- OUTPUT_PARSER/stroutputparser.py
+++ OUTPUT_PARSER/stroutputparser.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
@@ -14,18 +15,28 @@ llm = HuggingFaceEndpoint(
 model = ChatHuggingFace(llm=llm)
 
 
-prompt1 = PromptTemplate(
+template1 = PromptTemplate(
     template='Write a detailed report on {topic}',
     input_variables=['topic']
-).invoke({'topic':'blackhole'})
+)#.invoke({'topic':'blackhole'})
 
 
-result = model.invoke(prompt1)
+#result = model.invoke(prompt1)
 
-prompt2 = PromptTemplate(
+template2 = PromptTemplate(
     template='Write a summary on the following text. /n {text}',
     input_variables=['text']
-).invoke({'text':result.content})
+)#.invoke({'text':result.content})
+
+#result1 = model.invoke(prompt2)
 
 
-result1 = model.invoke(prompt2)
+
+parser = StrOutputParser()
+
+
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({'topic':'blackhole'})
+
+print(result)
